f357b10b5aa32af7f506ba31d3b91e7d25ebd83f
Rosario <rosario7dev@gmail.com>
2024-09-12 22:28:59 -0400

    Creacion de componente character con signal

---
+++ src/app/components/character/character/character.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { NavComponent } from "../../../paqges/nav/nav.component";
+import { CharacterService } from '../../../services/character.service';
+import { Character } from '../../../model/character.model';
+import { LoadingComponent } from "../../loading/loading.component";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-character',
+  standalone: true,
+  imports: [MatCardModule, NavComponent, LoadingComponent],
+  templateUrl: './character.component.html',
+  styleUrl: './character.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CharacterComponent implements OnInit {
+
+  characterService = inject(CharacterService);
+  router = inject(Router);
+
+  ngOnInit(): void {
+
+  }
+
+  return() {
+    this.router.navigate(['/characters']);
+  }
+
+}
+
+
